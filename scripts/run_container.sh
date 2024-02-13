96363f7fc24c0d3d881c45606eaf90f2de11a7c2
9elmo6 <am.elmoghazy@gmail.com>
2024-02-13 18:08:14 -0500

    upgraded to humble

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # create workspace on host, in home directory
-mkdir -p /Users/elmoghazy96/Projects/IoTLabTenth/f1tenth_ws
+mkdir -p $HOME/f1tenth_ws
 
 # give docker permission to use X
 sudo xhost +si:localuser:root
 
 # run container with privilege mode, host network, display, and mount workspace on host
-sudo docker run  -it --privileged --network host -e DISPLAY=$DISPLAY \
+sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev \
-    --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=/Users/elmoghazy96/Projects/IoTLabTenth/f1tenth_ws \
-    theelmo96/f1tenth_upgrade:v1.3
+    --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
+    theelmo96/f1tenth_upgrade:humble
